b81723a3e86e66efe9686023407c6ddf475c83ec
Nazareno Franco <nazyfranco@gmail.com>
2025-05-20 22:59:15 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -10,14 +10,18 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    private Double score;
 
     @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platforms;
-    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
